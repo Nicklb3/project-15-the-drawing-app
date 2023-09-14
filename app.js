06d33c3331bf8d961e3e1c0415e53da40dcc8f09
Nicholas Bonner <nicholasbonner@Nicholass-Mac-mini.local>
2023-09-13 23:12:48 -0700

    added all variables needed

---
+++ app.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('drawing-canvas');
+const increaseBtn = document.querySelector('#increase');
+const decreseBtn = document.querySelector('#decrese');
+const strokeThinkness = document.querySelector('#size');
+const colorBtn = document.querySelector('#color');
+const clearBtn = document.querySelector('#clear');
